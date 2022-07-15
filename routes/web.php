4a278a291662d937cee4072210bece82e5d14a5c
zeeshan_k <zeeshan@searcheye.io>
2022-07-15 18:21:59 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Auth::routes();
+
+
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth'], function () {
+    Route::get('home', 'HomeController@index')->name('home');
+	Route::resource('user', 'UserController', ['except' => ['show']]);
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+	Route::get('upgrade', function () {return view('pages.upgrade');})->name('upgrade');
+	 Route::get('map', function () {return view('pages.maps');})->name('map');
+	 Route::get('icons', function () {return view('pages.icons');})->name('icons');
+	 Route::get('table-list', function () {return view('pages.tables');})->name('table');
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+});
+
+
